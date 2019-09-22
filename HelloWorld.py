223068d5ee64330350e9077c1dc4203c2e387227
HabibiRam123 <ramdevakumar20@gmail.com>
2019-09-22 11:50:06 -0400

    hi

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,11 +2,16 @@ print("Hello World")
 print("We are currently using github")
 
 import random
+def cd_gen(times = 1):
+    for x in range(times):
+        a =(random.random())
+        b = (random.randint(40,100))
 
-a =(random.random())
-b = (random.randint(40,100))
+        c = (round(a + b, 2))
 
-c = (round(a + b, 2))
+        cd_list = ["N", "E", "S", "W", "SE", "SW", "NW", "NE"]
 
-cd = ["N", "E", ""]
-
+        cd = random.choice(cd_list)
+        return(str(c) + "°" + cd)
+cd_gen(5)
+print("Food is currently being sent to",cd_gen(1))
