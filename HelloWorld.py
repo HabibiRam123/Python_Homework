2535d8ffca21cb9cea5c6731b4d7c9288caa3585
HabibiRam123 <ramdevakumar20@gmail.com>
2019-11-16 12:46:59 -0500

    hi

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,5 @@
+
+'''
 print("Hello World")
 print("We are currently using github")
 
@@ -15,3 +17,61 @@ def cd_gen(times = 1):
         return(str(c) + "°" + cd)
 cd_gen(5)
 print("Food is currently being sent to",cd_gen(1))
+'''
+
+
+# QUESTION 1
+'''
+def rotate(a, b, c):
+    list1 = [a, b, c]
+    x = (list1.pop(0))
+    list1.append(int(x))
+    return (list1)
+#list2 = list1.append(x)
+#print(list2)
+print(rotate(1,2,3))
+'''
+
+# QUESTION 4
+'''
+def LastTwo(string):
+    string = list(string)
+    x = string.pop(-2)
+    string.append(x)
+    print("".join(string))
+LastTwo("cat")
+'''
+
+# QUESTION 3
+'''
+def ShiftLeft(intlist):
+    intlist = list(intlist)
+    lst = []
+    for x in intlist:
+        lst.append(int(x))
+    x = lst.pop(0)
+    lst.append(x)
+    print(lst)
+ShiftLeft('12433423')
+'''
+
+# QUESTION 2
+'''
+a = [1,2,3,4,5,3,4,3]
+
+y = 0
+for x in a:
+    if x == 3:
+        y += 1
+        index = a.index(x)
+        print(index)
+
+print(y)
+'''
+
+#hi
+my_list = [10,2,3,5,2,1,6,3,5,2,2]
+a = 2# element to be found
+for x in range(len(my_list)):#traversing through length of the list
+    if my_list[x] == a:
+        print(x)
